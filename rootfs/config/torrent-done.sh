44cf08bfaaa8f4c08719fd68a909c54c1f33db98
Igor Vinokurov <zynaps@zynaps.ru>
2019-02-01 10:19:38 +0300

    move downloaded single file movie

--- rootfs/config/torrent-done.sh
+++ rootfs/config/torrent-done.sh
@@ -4,4 +4,8 @@ set -xe
 
 env
 
+[[ -d "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ]] && exit
+
+mv -i -f "$TR_TORRENT_DIR/$TR_TORRENT_NAME" /plex/data/movies
+
 transmission-remote --torrent $TR_TORRENT_ID --remove
