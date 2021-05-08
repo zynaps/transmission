5466726ba069a51806498d672f9460dc9bb7be1c
Igor Vinokurov <zynaps@zynaps.ru>
2021-05-08 13:56:43 +0400

    more clearer torrent-done script

--- rootfs/config/torrent-done.sh
+++ rootfs/config/torrent-done.sh
@@ -5,11 +5,13 @@ set -xe
 env
 
 if [[ -d "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ]]; then
-    [[ $(ls "$TR_TORRENT_DIR/$TR_TORRENT_NAME/"*.{avi,srt} | wc -l) -ne "2" ]] && exit
-
-    mv -i -f "$TR_TORRENT_DIR/$TR_TORRENT_NAME/"*.{avi,srt} /plex/data/movies
+    if [[ $(ls "$TR_TORRENT_DIR/$TR_TORRENT_NAME/"*.{avi,srt} | wc -l) -ne "2" ]]; then
+      exit
+    else
+      mv -f "$TR_TORRENT_DIR/$TR_TORRENT_NAME/"*.{avi,srt} /movies
+    fi
 else
-    mv -i -f "$TR_TORRENT_DIR/$TR_TORRENT_NAME" /plex/data/movies
+    mv -f "$TR_TORRENT_DIR/$TR_TORRENT_NAME" /movies
 fi
 
 transmission-remote --torrent $TR_TORRENT_ID --remove-and-delete
